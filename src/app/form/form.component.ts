bdc57b29823df928799b079b497a8fc866478aae
sushilganvir <sushilganvir123@gmail.com>
2022-07-28 10:54:10 +0530

    response error handling added

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,38 +1,25 @@
-import { Component, OnInit, Output , EventEmitter  } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { UserServiceService } from '../user-service.service';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+  @Output() form: EventEmitter<any> = new EventEmitter();
 
-  @Output() form:EventEmitter<any> = new EventEmitter();
+  title: string = '';
+  body: string = '';
 
-  title:string = '';
-  body:string = '';
+  constructor(public userData: UserServiceService) {}
 
+  ngOnInit(): void {}
 
-
-  constructor(public userData:UserServiceService) { }
-
-  ngOnInit(): void {
-
-
-
+  addUser(user: any) {
+    this.userData.addUser(user).subscribe((newUser) => {
+      this.form.emit(newUser);
+      this.userData.showModel = false;
+    });
   }
-
-  onUserAdded(){}
-
-  addUser(user:any){
-    this.userData.addUser(user).subscribe(newUser => {
-      this.form.emit(newUser)
-
-    })
-  }
-
- 
-
-
 }
--- src/app/user-data/user-data.component.ts
+++ src/app/user-data/user-data.component.ts
@@ -11,12 +11,18 @@ export class UserDataComponent implements OnInit {
 
   userList:any;
   newUser:any;
+  error: string = ''
+  
 
   constructor(public user:UserServiceService) { }
 
   ngOnInit(): void {
     this.user.getUser().subscribe(data => {
       this.userList = data
+
+    }, (error ) => {
+      if(error.status === 404)
+     this.error = 'no data available'
     })
 
 
